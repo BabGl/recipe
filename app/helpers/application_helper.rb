cdbf2cf9040d15997171603ea5e54f8a943270ad
babgl <ningarhar@outlook.com>
2016-02-03 00:57:53 +0000

    Gravatar toegevoegd

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  
+  def gravatar_for(chef, options = {size: 80})
+    gravatar_id = Digest::MD5::hexdigest(chef.email.downcase)
+    size = option[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: chef.chefname, class: "gravatar")
+
+#image_tag("http://www.gravatar.com/avatar.php?gravatar_id=#{Digest::MD5::hexdigest(@user.email)}", 
+#:alt => 'Avatar', :class => 'avatar')
+  end
+  
+  
+  
 end
