a822505e4d72c406e321a424023201415d291cbd
babgl <ningarhar@outlook.com>
2016-02-16 22:31:38 +0000

    create new and edit forms and partials

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,7 @@ class RecipesController < ApplicationController
   end
   
   def show
-    @recipes = Recipe.find(params[:recipe_id])
+    @recipe = Recipe.find(params[:id])
   end
   
   def new
@@ -17,13 +17,29 @@ class RecipesController < ApplicationController
     @recipe.chef = Chef.find(2)
     
     if @recipe.save
-      flash[:success] = "Your recipe was created succesfully"
+      flash[:success] = "Uw Recept is aangemaakt"
       redirect_to recipes_path
       elsif render :new
     end
-        
     
-    
+  end
+   def edit
+        @recipe = Recipe.find(params[:id])
+   end
+  def update
+        @recipe = Recipe.find(params[:id])
+        if @recipe.update(recipe_params)
+        flash[:Succes] = "Uw Recept is bewerkt"
+        redirect_to recipes_path(@recipe)
+        else
+          render :edit
+        end
+  end
+  
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recept verwijderd"
+    redirect_to recipes_path
   end
   
   private
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,9 +2,9 @@ class Recipe < ActiveRecord::Base
  belongs_to :chef
  #validates :recipe, presence: true
 # validates :chef_id, presence: true
-  #validates :name, presence: true, length: {minimum: 5, maximum: 100}
-  #validates :summary, presence: true, length: {minimum: 10, maximum: 151}
-   #validates :description, presence: true, length: {minimum: 20, maximum: 500}
+  validates :name, presence: true, length: {minimum: 5, maximum: 100}
+  validates :summary, presence: true, length: {minimum: 10, maximum: 151}
+   validates :description, presence: true, length: {minimum: 20, maximum: 500}
    #validates :recepten, presence: true
   #validates :chef_id, presence: true
   #validates :naam, presence: true
